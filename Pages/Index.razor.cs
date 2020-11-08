ce8e0a598b662ee6325050d1577019b248bad420
taher fattahi <taherfattahi11@gmail.com>
2020-11-08 18:59:18 +0330

    init commit

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorRangeSlider.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject]
+        IJSRuntime js { get; set; }
+
+        [Inject]
+        ILogger<Index> logger { get; set; }
+
+        public static Index IndexInstance;
+
+        protected override async Task OnInitializedAsync()
+        {
+            IndexInstance = this;
+        }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await js.InvokeVoidAsync("initJsFunctionsFromCsharp");
+        }
+
+        [JSInvokable]
+        public async static void onStart(string data)
+        {
+            //logger.LogWarning(e.ChangeType.ToString());
+            IndexInstance.logger.LogWarning(data + " logger");
+        }
+
+        [JSInvokable]
+        public async static void onChange(string data)
+        {
+            IndexInstance.logger.LogWarning(data + " logger");
+        }
+
+        [JSInvokable]
+        public async static void onFinish(string data)
+        {
+            IndexInstance.logger.LogWarning(data + " logger");
+        }
+
+
+        [JSInvokable]
+        public async static void onUpdate(string data)
+        {
+            IndexInstance.logger.LogWarning(data + " logger");
+
+        }
+
+    }
+}
